d03b2fea8b941b6fa4633e3b0826bac680d76336
KuangXiangGan <kuangxianggan@bz.cn>
2018-12-17 20:48:37 +0800

    第一个需求处理完成

--- src/main/java/com/bz/git/HelloGit.java
+++ src/main/java/com/bz/git/HelloGit.java
@@ -4,6 +4,12 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		System.out.println("第一个需求处理中");
+		for(int i=0;i<10;i++) {
+			System.out.println("---");
+		}
+		System.out.println("第一个需求处理完成");
+		
 	}
 
 }
